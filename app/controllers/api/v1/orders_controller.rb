dba7ce62b3b52f380efe6c2880b443df6a8f88f1
Nicolas Heinig <nicolas.heinig@xing.com>
2020-03-21 15:09:55 +0100

    Dont required auth for get calls

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::OrdersController < ApplicationController
-  # skip_before_action :authenticate_user!
+  skip_before_action :authenticate_user!, only: %i(index show)
   skip_before_action :verify_authenticity_token, if: ->{ request.format.json? }
 
   def index
