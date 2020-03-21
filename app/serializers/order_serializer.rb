8627aa79d7fea3aef76d8a387630fa7b1748c958
Nicolas Heinig <nicolas.heinig@xing.com>
2020-03-21 12:52:59 +0100

    Add total to orders

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,5 +1,5 @@
 class OrderSerializer < ActiveModel::Serializer
-  attributes :id, :list, :status
+  attributes :id, :list, :status, :total
   belongs_to :owner, serializer: UserSerializer
   belongs_to :shopper, serializer: UserSerializer
 end
