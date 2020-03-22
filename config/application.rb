8ce55c37d785eb6f6929120938044ba400985050
Nicolas Heinig <nicolas.heinig@xing.com>
2020-03-22 17:32:25 +0100

    Amtssprache ist deutsch

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,8 @@ module OmaTelefon
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.i18n.enforce_available_locales = false
+    config.i18n.available_locales = ["de_DE", :en]
+    config.i18n.default_locale = 'de-DE'
   end
 end
